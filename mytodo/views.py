c31bd0c057564d1e76f6e430f8d91d664b11cabc
nishtha <nishthathakkar0502@gmail.com>
2020-11-21 10:36:37 +0530

    todo app

--- mytodo/views.py
+++ mytodo/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from mytodo.models import ToDo
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html',context={'app_name':"ToDo App"})
+    template_name = 'home.html'
+    todo_list = ToDo.objects.all()
+    #print(todo_list)
+    context = {'app_name':'todoapp','todo_list':todo_list}
+    return render(request,template_name,context=context)
